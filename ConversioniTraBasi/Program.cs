db02c87bddb7f8afd6915a33fe2c720db7c11a39
simone700 <patrunosimone67@gmail.com>
2020-10-17 09:27:21 +0200

    Creazione di un progetto che converte da decimale a base 2, 8, 16

---
+++ ConversioniTraBasi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConversioniTraBasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resto, numero, valorebase;
+            string valore_convertito = "";
+            Console.WriteLine("inserisci la base in cui vuoi convertire");
+            valorebase = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci il numero da convertire");
+            numero = Convert.ToInt32(Console.ReadLine());
+            while (numero > 0)
+            {
+                resto = numero % valorebase;
+                if (resto == 15)
+                {
+
+                    valore_convertito = "f" + valore_convertito;
+                }
+                if (resto == 14)
+                {
+
+                    valore_convertito = "f" + valore_convertito;
+                }
+                numero = numero / valorebase;
+                valore_convertito = Convert.ToString(resto) + valore_convertito;
+            }
+            Console.WriteLine($"il numero binario é {valore_convertito}");
+            Console.ReadLine();
+        }
+    }
+}
